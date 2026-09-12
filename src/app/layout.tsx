3e604c669e3b58f8dd8d7d15d28a1c58b823c5da
rothi <hasanrothi99@gmail.com>
2026-09-12 12:27:29 +0600

    fix: build issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="antialiased">{children}</body>
+      <body className="antialiased" suppressHydrationWarning>{children}</body>
     </html>
   );
 }
